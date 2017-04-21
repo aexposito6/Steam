a8674a959f33af5b94566c2987fc034876a06386
frandolz <frandolzsallen@gmail.com>
2017-04-21 16:26:16 +0200

    Details of model

--- Steam/steam_app/models.py
+++ Steam/steam_app/models.py
@@ -6,43 +6,43 @@ from django.contrib.auth.models import User
 import json
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(User)
-    steam_id = models.IntegerField(blank=True, null = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    steam_id = models.IntegerField(blank=True, null = True, unique=True)
     name = models.CharField(max_length=50, blank=True, null=True)
     real_name=models.CharField(max_length=50, blank=True, null=True)
     country=models.CharField(max_length=50, blank=True, null=True)
     url_profile=models.CharField(max_length=50, blank=True, null=True)
     avatar=models.CharField(max_length=50, blank=True, null=True)
-    friends=models.ManyToManyField('self', null=True, blank=True)
+    friends=models.TextField(null=True, blank=True) #Para poder poner amigos que no esten registrados a nuestra app
 
     def __unicode__(self):
         return str(self.user.username)
 
 class Game (models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(UserProfile)
     appid = models.IntegerField(blank=True,  null = True)
     name = models.CharField(max_length=50, blank=True, null=True)
     version= models.FloatField(blank=True, null=True)
     company=models.CharField(max_length=50, blank=True, null=True)
     news = models.TextField(null=True, blank=True)
     def __unicode__(self):
-        return str("Usuario: " + self.user.username + "," + "Juego: " + str(self.name))
+        return str("Usuario: " + str(self.user) + "," + "Juego: " + str(self.name))
 
 
 class Achievement(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(UserProfile)
     appid_game = models.ForeignKey(Game, blank=True, null=True)
     name = models.CharField(max_length=50, blank=True, null=True)
     achieved = models.BooleanField(default=False)
     def __unicode__(self):
-        return str("Usuario: " + self.user.username + "," + "Achievement: "+ self.name)
+        return str("Usuario: " + str(self.user) + "," + "Achievement: "+ str(self.name))
 
 
 class Clan(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(UserProfile)
     id_clan=models.IntegerField(blank=True, null=True)
     name = models.CharField(max_length=50, blank=True, null=True)
     number_of_person=models.IntegerField(blank=True, null=True)
-    members = models.TextField(null=True, blank=True)
+    members = models.TextField(null=True, blank=True) #Para poder poner miembros que no esten regitrados a nuestra app
     def __unicode__(self):
         return str(self.name)
